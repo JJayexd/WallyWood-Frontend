5363a521e5916dbdfb66dd75bdb8a31bb270bbbc
JJayexd <kontakt.emilbj@gmail.com>
2024-11-26 13:04:11 +0100

    t

---
+++ Assets/JS/main.js
@@ -0,0 +1,29 @@
+const ROOT = document.getElementById('root')
+
+const ul = document.createElement('ul')
+const navItems = ['WALLYWOOD', 'FORSIDE', 'PLAKATER', 'OM OS', 'KONTAKT', 'LOGIN', 'BASKET']
+
+navItems.forEach((item, index) => {
+    const li = document.createElement('li');
+
+    li.textContent = item;
+    li.href = `${item}`;
+
+    if (index > 5 ) {
+        li.innerHTML = '<i class="fa-solid fa-basket-shopping" id="basket"></i>'
+    }
+
+    if (index === 0 ) {
+        li.classList.add('bold')
+    }
+
+    ul.appendChild(li)
+    ROOT.appendChild(ul)
+})
+
+const img = document.createElement('img')
+img.src = 'Assets/image.png'
+
+img.width = 1300
+
+ROOT.appendChild(img)
